8fd76900e2446be3d77b34014f45eb30ea04033e
SMHRD <SMHRD@DESKTOP-Q72UU17>
2022-02-14 16:46:19 +0900

    두번쨰 커밋!

--- GItTest/src/GitTest01.java
+++ GItTest/src/GitTest01.java
@@ -4,6 +4,9 @@ public class GitTest01 {
 	public static void main(String[] args) {
 		
 		System.out.println("Ã¹¹øÂ° Ä¿¹Ô!");
+		System.out.println("µÎ¹ø¤Š Ä¿¹Ô!");
+		
+		System.out.println("3ÆÀ Ä¿¹Ô¿Ï·á");
 	}
 
 }
