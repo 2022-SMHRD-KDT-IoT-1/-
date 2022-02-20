74b77cba0518c0f0da84cb0fbd3cdc63a5efe288
SMHRD <SMHRD@DESKTOP-4RRQ889>
2022-02-20 16:48:54 +0900

    혁준

--- GItTest/src/GitTest01.java
+++ GItTest/src/GitTest01.java
@@ -4,12 +4,9 @@ public class GitTest01 {
 	public static void main(String[] args) {
 		
 		System.out.println("Ã¹¹øÂ° Ä¿¹Ô!");
-<<<<<<< HEAD
-		System.out.println("µÎ¹ø¤Š Ä¿¹Ô!");	
-=======
 
-		System.out.println("µÎ¹ø¤Š Ä¿¹Ô!");
->>>>>>> branch 'master' of https://github.com/2022-SMHRD-KDT-IoT-1/-.git
+		System.out.println("µÎ¹ø¤Š Ä¿¹Ô!");	
+
 		
 		System.out.println("3ÆÀ Ä¿¹Ô¿Ï·á");
 	}
